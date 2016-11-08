7ac21c4b19316a64f9e2a722b2375505cbc0fa9d
Brandon Rowe <browe14@mail.fresnostate.edu>
2016-11-08 14:42:00 -0800

    Updated tables

--- db_files/FresnoStateBuyNSell.sql
+++ db_files/FresnoStateBuyNSell.sql
@@ -7,13 +7,26 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 
+CREATE TABLE `categories` (
+  `CategoryID` int(11) NOT NULL,
+  `CategoryName` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `comments` (
+  `ProductID` int(11) NOT NULL,
+  `UserID` int(11) NOT NULL,
+  `CommentID` int(11) NOT NULL,
+  `Comment` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE `products` (
   `ProductID` int(11) NOT NULL,
   `UserID` int(11) NOT NULL,
   `ProductName` varchar(50) NOT NULL,
+  `CategoryID` int(11) NOT NULL,
   `Price` int(11) NOT NULL,
-  `PostTime` int(11) NOT NULL,
-  `Description` varchar(150) NOT NULL,
+  `PostTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Description` varchar(500) DEFAULT NULL,
   `PicturePath` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -25,6 +38,14 @@ CREATE TABLE `users` (
   `Password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`CategoryID`),
+  ADD UNIQUE KEY `CategoryID` (`CategoryID`);
+
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`CommentID`),
+  ADD UNIQUE KEY `CommentID` (`CommentID`);
 
 ALTER TABLE `products`
   ADD PRIMARY KEY (`ProductID`),
