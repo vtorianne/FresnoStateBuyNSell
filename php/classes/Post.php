334dc0ca0b799709e87318e5543aacf5d042c364
Carlos <cdaccarett93@gmail.com>
2017-03-08 10:45:12 -0800

    fixed branch because of merge having mixed up old files/code with new files/code

--- php/classes/Post.php
+++ php/classes/Post.php
@@ -1,9 +1,9 @@
 <?php
-    ini_set('display_errors',1); 
+    ini_set('display_errors',1);
     error_reporting(E_ALL);
     require_once "DB.php";
     class Post{
-        
+
         public function getPosts(){
             if(isset($_POST['searchSubmit'])){
                 $filters = array();
@@ -36,7 +36,7 @@
             require_once "views/listings.php";
             require_once "../html/footer.html"; //footer
         }
-        
+
         public function getPostDetails(){
             $db = new DB();
             $postID = $_GET["post-id"];
@@ -50,6 +50,7 @@
             require_once "views/listing.php";
             require_once "../html/footer.html"; //footer
         }
+
         public function getFilteredQuery($filters){
             if($filters == NULL) {
                 $sql = "SELECT * FROM products ORDER BY PostTime DESC;";
@@ -81,8 +82,10 @@
                     }
                     $searchfilterscount++;
                 }
+
                 if($searchfilterscount == 0)
                     $sql.= " 1";
+
                 if(array_key_exists("priceSort", $filters)){
                     $sql .= " ORDER BY Price ASC, PostTime DESC";
                 }
@@ -93,6 +96,7 @@
             }
             return $sql;
         }
+
         public function getPostCategories(){
             $db = new DB();
             $sql = "SELECT * FROM categories;";
@@ -107,7 +111,7 @@
             }
             return $categories;
         }
-        
+
         public function createPost(){
             $db = new DB();
             $target_file = "/uploads/listing_pics/".basename($_FILES["pic"]["name"]);
@@ -122,7 +126,7 @@
             $sql = "INSERT INTO products (UserID, ProductName, CategoryID, Price, Description, PicturePath) VALUES ($userID, '$productname', $categoryID, $price, '$description', '$picturepath'); "; //insert new posts
             $db->execute($sql);
         }
-        
+
         public function markSold(){
             $postID = $_GET["post-id"];
             $currUserID = $_SESSION["Current_User"];
@@ -138,7 +142,7 @@
                 return true;
             }
         }
-        
+
         public function addComment(){
             $db = new DB();
             $currUserID = $_SESSION["Current_User"];
@@ -147,7 +151,7 @@
             $sql = "INSERT INTO comments (ProductID, UserID, Comment) VALUES ($postID, $currUserID, '$comment');";
             $db->execute($sql);
         }
-        
+
         public function getComments($postID){
             $db = new DB();
             $sql = "SELECT * FROM comments WHERE ProductID = $postID ORDER BY CommentTimeStamp ASC;"; //get all comments for a post
@@ -167,5 +171,5 @@
             }
             return $comments;
         }
-        
+
     }
